74e3141ace501c941065913612b0a3bee8377769
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2023-03-28 23:44:15 -0600

    Starting project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return <div>This is the Home Page</div>;
+  };
+export const Home = () => {
+    return <div>This is the Other Page</div>;
+  };
+  export const NotFound = () => {
+    return <div>Not Found</div>;
+  };
