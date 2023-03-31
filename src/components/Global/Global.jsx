0839b890f79202a00b62b9b4beac96645cd5d83b
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2023-03-31 14:18:07 -0600

    header almost finished

---
+++ src/components/Global/Global.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+import React from 'react'
+import Header from '../Header/Header'
+
+function Global() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Global
